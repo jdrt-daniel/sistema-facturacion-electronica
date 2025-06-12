cd5dc5a249424b5d17f9af6fd8e6bbc4b83dcc05
Daniel Rafael <jdrt.1012@gmail.com>
2025-06-11 22:45:49 -0400

    se limpiaron los comentario de los archivos

--- app/views/layout/app.layout.php
+++ app/views/layout/app.layout.php
@@ -1,13 +1,11 @@
 <!doctype html>
 <html lang="en">
-<!--begin::Head-->
 
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
   <title>
     <?php echo $_ENV['APP_NAME']; ?>
   </title>
-  <!--begin::Primary Meta Tags-->
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <meta name="title" content="AdminLTE | Dashboard v3" />
   <meta name="author" content="ColorlibHQ" />
@@ -17,34 +15,23 @@
   <meta
     name="keywords"
     content="bootstrap 5, bootstrap, bootstrap 5 admin dashboard, bootstrap 5 dashboard, bootstrap 5 charts, bootstrap 5 calendar, bootstrap 5 datepicker, bootstrap 5 tables, bootstrap 5 datatable, vanilla js datatable, colorlibhq, colorlibhq dashboard, colorlibhq admin dashboard" />
-  <!--end::Primary Meta Tags-->
-  <!--begin::Fonts-->
   <link
     rel="stylesheet"
     href="https://cdn.jsdelivr.net/npm/@fontsource/source-sans-3@5.0.12/index.css"
     integrity="sha256-tXJfXfp6Ewt1ilPzLDtQnJV4hclT9XuaZUKyUvmyr+Q="
     crossorigin="anonymous" />
-  <!--end::Fonts-->
-  <!--begin::Third Party Plugin(OverlayScrollbars)-->
   <link
     rel="stylesheet"
     href="https://cdn.jsdelivr.net/npm/overlayscrollbars@2.10.1/styles/overlayscrollbars.min.css"
     integrity="sha256-tZHrRjVqNSRyWg2wbppGnT833E/Ys0DHWGwT04GiqQg="
     crossorigin="anonymous" />
-  <!--end::Third Party Plugin(OverlayScrollbars)-->
-  <!--begin::Third Party Plugin(Bootstrap Icons)-->
   <link
     rel="stylesheet"
     href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"
     integrity="sha256-9kPW/n5nn53j4WMRYAxe9c1rCY96Oogo/MKSVdKzPmI="
     crossorigin="anonymous" />
-
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
-  <!--end::Third Party Plugin(Bootstrap Icons)-->
-  <!--begin::Required Plugin(AdminLTE)-->
   <link rel="stylesheet" href="/assets/css/adminlte.css" />
-  <!--end::Required Plugin(AdminLTE)-->
-  <!-- apexcharts -->
   <link
     rel="stylesheet"
     href="https://cdn.jsdelivr.net/npm/apexcharts@3.37.1/dist/apexcharts.css"
@@ -53,13 +40,9 @@
 </head>
 
 <body class="layout-fixed sidebar-expand-lg bg-body-tertiary">
-  <!--begin::App Wrapper-->
   <div class="app-wrapper">
-    <!--begin::Header-->
     <nav class="app-header navbar navbar-expand bg-body">
-      <!--begin::Container-->
       <div class="container-fluid">
-        <!--begin::Start Navbar Links-->
         <ul class="navbar-nav">
           <li class="nav-item">
             <a class="nav-link" data-lte-toggle="sidebar" href="#" role="button">
@@ -67,17 +50,7 @@
             </a>
           </li>
         </ul>
-        <!--end::Start Navbar Links-->
-        <!--begin::End Navbar Links-->
         <ul class="navbar-nav ms-auto">
-          <!--begin::Navbar Search-->
-          <li class="nav-item">
-            <a class="nav-link" data-widget="navbar-search" href="#" role="button">
-              <i class="bi bi-search"></i>
-            </a>
-          </li>
-          <!--end::Navbar Search-->
-          <!--begin::Notifications Dropdown Menu-->
           <li class="nav-item dropdown">
             <a class="nav-link" data-bs-toggle="dropdown" href="#">
               <i class="bi bi-bell-fill"></i>
@@ -104,16 +77,12 @@
               <a href="javascript:void(0)" class="dropdown-item dropdown-footer"> See All Notifications </a>
             </div>
           </li>
-          <!--end::Notifications Dropdown Menu-->
-          <!--begin::Fullscreen Toggle-->
           <li class="nav-item">
             <a class="nav-link" href="#" data-lte-toggle="fullscreen">
               <i data-lte-icon="maximize" class="bi bi-arrows-fullscreen"></i>
               <i data-lte-icon="minimize" class="bi bi-fullscreen-exit" style="display: none"></i>
             </a>
           </li>
-          <!--end::Fullscreen Toggle-->
-          <!--begin::User Menu Dropdown-->
           <li class="nav-item dropdown user-menu">
             <a href="javascript:void(0)" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
               <img
@@ -125,7 +94,6 @@
               </span>
             </a>
             <ul class="dropdown-menu dropdown-menu-lg dropdown-menu-end">
-              <!--begin::User Image-->
               <li class="user-header text-bg-light">
                 <img
                   src="/assets/assets/img/user2-160x160.jpg"
@@ -140,43 +108,26 @@
               </li>
               <li class="user-footer">
                 <a href="javascript:void(0)" class="btn btn-default btn-flat">Perfil</a>
-                <a href="javascript:void(0)" class="btn btn-default btn-flat float-end"
-
+                <a href="javascript:void(0)" class="btn btn-dark btn-flat float-end"
                   onclick="logout()">Salir</a>
               </li>
-              <!--end::Menu Footer-->
             </ul>
           </li>
-          <!--end::User Menu Dropdown-->
         </ul>
-        <!--end::End Navbar Links-->
       </div>
-      <!--end::Container-->
     </nav>
-    <!--end::Header-->
-    <!--begin::Sidebar-->
     <aside class="app-sidebar bg-body-secondary shadow" data-bs-theme="dark">
-      <!--begin::Sidebar Brand-->
       <div class="sidebar-brand">
-        <!--begin::Brand Link-->
         <a href="/" class="brand-link">
-          <!--begin::Brand Image-->
           <img
             src="/assets/assets/img/AdminLTELogo.png"
             alt="AdminLTE Logo"
             class="brand-image opacity-75 shadow" />
-          <!--end::Brand Image-->
-          <!--begin::Brand Text-->
           <span class="brand-text fw-light">Sistema ventas</span>
-          <!--end::Brand Text-->
         </a>
-        <!--end::Brand Link-->
       </div>
-      <!--end::Sidebar Brand-->
-      <!--begin::Sidebar Wrapper-->
       <div class="sidebar-wrapper">
         <nav class="mt-2">
-          <!--begin::Sidebar Menu-->
           <ul
             class="nav sidebar-menu flex-column"
             data-lte-toggle="treeview"
@@ -272,87 +223,51 @@
                 </li>
               </ul>
             </li>
-
           </ul>
-          <!--end::Sidebar Menu-->
         </nav>
       </div>
-      <!--end::Sidebar Wrapper-->
     </aside>
-    <!--end::Sidebar-->
-    <!--begin::App Main-->
     <main class="app-main">
-      <!--begin::App Content Header-->
       <div class="app-content-header">
-        <!--begin::Container-->
         <div class="container-fluid">
-          <!--begin::Row-->
           <div class="row">
             <div class="col-sm-6">
               <h3 class="mb-0">
                 <?php echo $title ?? ''; ?>
               </h3>
             </div>
-            <!-- <div class="col-sm-6">
-              <ol class="breadcrumb float-sm-end">
-                <li class="breadcrumb-item"><a href="javascript:void(0)">Home</a></li>
-                <li class="breadcrumb-item active" aria-current="page">Dashboard v3</li>
-              </ol>
-            </div> -->
           </div>
-          <!--end::Row-->
         </div>
-        <!--end::Container-->
       </div>
       <div class="app-content">
-        <!--begin::Container-->
         <div class="container-fluid">
-          <!--begin::Row-->
           <div class="row">
             <?php
             echo $content;
             ?>
-
           </div>
-          <!--end::Row-->
         </div>
-        <!--end::Container-->
       </div>
-      <!--end::App Content-->
     </main>
-    <!--end::App Main-->
-    <!--begin::Footer-->
     <footer class="app-footer">
-      <!--begin::To the end-->
       <div class="float-end d-none d-sm-inline">Anything you want</div>
-      <!--end::To the end-->
-      <!--begin::Copyright-->
       <strong>
         Copyright &copy; 2014-2024&nbsp;
         <a href="https://adminlte.io" class="text-decoration-none">AdminLTE.io</a>.
       </strong>
       All rights reserved.
-      <!--end::Copyright-->
     </footer>
-    <!--end::Footer-->
   </div>
-  <!--end::App Wrapper-->
-  <!--begin::Script-->
-  <!--begin::Third Party Plugin(OverlayScrollbars)-->
   <script
     src="https://cdn.jsdelivr.net/npm/overlayscrollbars@2.10.1/browser/overlayscrollbars.browser.es6.min.js"
     integrity="sha256-dghWARbRe2eLlIJ56wNB+b760ywulqK3DzZYEpsg2fQ="
     crossorigin="anonymous"></script>
-  <!--end::Third Party Plugin(OverlayScrollbars)--><!--begin::Required Plugin(popperjs for Bootstrap 5)-->
   <script
     src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
     integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
     crossorigin="anonymous"></script>
-
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
-  <!--end::Required Plugin(Bootstrap 5)--><!--begin::Required Plugin(AdminLTE)-->
   <script src="/assets/js/adminlte.js"></script>
-  <!--end::Required Plugin(AdminLTE)--><!--begin::OverlayScrollbars Configure-->
   <script>
     const SELECTOR_SIDEBAR_WRAPPER = '.sidebar-wrapper';
     const Default = {
@@ -373,9 +288,6 @@
       }
     });
   </script>
-  <!--end::OverlayScrollbars Configure-->
-  <!-- OPTIONAL SCRIPTS -->
-  <!-- apexcharts -->
   <script
     src="https://cdn.jsdelivr.net/npm/apexcharts@3.37.1/dist/apexcharts.min.js"
     integrity="sha256-+vh8GkaU7C9/wbSLIcwq82tQ2wTf44aOHA8HlBMwRI8="
@@ -390,7 +302,6 @@
       <?php echo $javascript; ?>
     </script>
   <?php endif; ?>
-  <!--end::Script-->
   <script>
     function logout() {
       fetch("/logout", {
@@ -408,6 +319,5 @@
     }
   </script>
 </body>
-<!--end::Body-->
 
 </html>
